3b58d5158f50d0b9d51fdc9cbc5bca626c061648
Jeff Rankin <rankin6@llnl.gov>
2013-05-07 07:25:56 -0700

    "finish move"

--- print.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import json
-
-response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
-# for tweet in response[results]:
-#     print tweet[text]
-
-print json.load(response)
