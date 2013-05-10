2344c9183ae1985eafb9212a52c8d2b16b33d03e
Jeff Rankin <jjrankin@gmail.com>
2013-05-09 19:54:55 -0700

    Reworked so its faster and correct

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -12,14 +12,16 @@ def loadSentiments(fp):
         sent, score = sentiment.strip().split("\t")
         sentiment_data[sent] = float(score)
 
-
-def loadTweets(fp):
+# Using score of the tweet for the basis for new terms
+def loadTweetsScore(fp):
+    new_terms_this_pass = []
     for line in fp:
         tweet = json.loads(line)
         # clean data before
         # find the tweet sentiment
         score = 0
         if tweet.get('text'):
+            new_terms_this_pass = []
             ttext = tweet.get('text').encode('utf-8').split()
             for i in range(len(ttext)):
                 if ttext[i] in sentiment_data:
@@ -27,12 +29,43 @@ def loadTweets(fp):
                 else:
                     if ttext[i] not in new_terms:
                         new_terms[ttext[i]] = 0
-                    new_terms[ttext[i]] += score
+                        new_terms_this_pass.append(ttext[i])
+            for j in range(len(new_terms_this_pass)):
+                new_terms[new_terms_this_pass[j]] += score
+
+
+def loadTweets(fp):
+    new_terms_this_pass = []
+    for line in fp:
+        tweet = json.loads(line)
+        # clean data before
+        # find the tweet sentiment
+        score = 0
+        pos_count = 0
+        neg_count = 0
+        if tweet.get('text'):
+            new_terms_this_pass = []
+            ttext = tweet.get('text').encode('utf-8').split()
+            for i in range(len(ttext)):
+                if ttext[i] in sentiment_data:
+                    score += sentiment_data[ttext[i]]
+                else:
+                    if ttext[i] not in new_terms:
+                        # print "New Term", ttext[i]
+                        counts = {'pcount': 1, 'ncount': 1}
+                        new_terms[ttext[i]] = counts
+                    new_terms_this_pass.append(ttext[i])
+            for j in range(len(new_terms_this_pass)):
+                # print "Score", score
+                if score > 0:
+                    new_terms[new_terms_this_pass[j]]['pcount'] += 1
+                else:
+                    new_terms[new_terms_this_pass[j]]['ncount'] += 1
 
 
 def printNewTerms():
     for key in new_terms:
-        format = "%s %f" % (key, float(new_terms[key]))
+        format = "%s %f" % (key, float(new_terms[key]['pcount']) / float(new_terms[key]['ncount']))
         print format
 
 
